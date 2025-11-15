961d2ce093ff66728acb5e3f436987c4917a0ce0
hiiiambobby <saba3st@gmail.com>
2025-11-15 16:32:06 +0330

    updated softmax

--- activation/softmax.py
+++ activation/softmax.py
@@ -11,23 +11,7 @@ def softmax(x: Tensor) -> Tensor:
         2. using matrix mul to do it :) (recommended)
     hint: a/b = a*(b^-1)
     """
-    x_exp = x.exp()
-    sum_ = x_exp @ Tensor(np.ones((x_exp.shape[-1], 1)))
-    softmax_result = x_exp * (sum_ ** -1)
-
-    if x.requires_grad:
-        def grad_fn(grad: np.ndarray):
-            s = softmax_result.data.reshape(-1, 1)  # column vector
-            # Jacobian of softmax: diag(s) - s*sᵀ
-            jac = np.diagflat(s) - s @ s.T
-            return jac @ grad.reshape(-1, 1)
-
-        depends_on = [Dependency(x, grad_fn)]
-    else:
-        depends_on = []
-
-    return Tensor(
-        data=softmax_result.data,
-        requires_grad=x.requires_grad,
-        depends_on=depends_on
-    )
+    exp_x = x.exp()
+    ones = Tensor(np.ones((exp_x.shape[-1], 1)))   # ← باید Tensor باشد
+    s = exp_x @ ones
+    return exp_x * (s ** -1)
