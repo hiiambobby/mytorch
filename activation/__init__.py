4e376f345cb23f5dfa80b3aa2c91e240c8b1e197
hiiiambobby <saba3st@gmail.com>
2025-12-07 15:49:27 +0330

    small change

--- activation/__init__.py
+++ activation/__init__.py
@@ -1,5 +1,5 @@
 from mytorch.activation.step import step 
-from mytorch.activation.relu import relu 
+from mytorch.activation.relu import ReLU 
 from mytorch.activation.leaky_relu import leaky_relu 
 from mytorch.activation.sigmoid import sigmoid 
 from mytorch.activation.softmax import softmax 
