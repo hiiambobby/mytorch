1332e80a7919b9b69edc3e7c274f8349f9096274
hiiiambobby <saba3st@gmail.com>
2025-12-11 23:03:06 +0330

    momentum added

--- optimizer/momentum.py
+++ optimizer/momentum.py
@@ -1,9 +1,43 @@
+from typing import List
+from mytorch.layer import Layer
 from mytorch.optimizer import Optimizer
+import numpy as np
 
 "TODO: (optional) implement Momentum optimizer"
+
 class Momentum(Optimizer):
-    def __init__(self):
-        pass
+    def __init__(self, layers: List[Layer], learning_rate=0.01, momentum=0.9):
+        super().__init__(layers)
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.velocities = {}
 
     def step(self):
-        pass
+        for layer in self.layers:
+            if layer not in self.velocities:
+                self.velocities[layer] = {
+                    'w': np.zeros_like(layer.weight.data) if layer.weight is not None else None,
+                    'b': np.zeros_like(layer.bias.data) if layer.need_bias and layer.bias is not None else None
+                }
+            
+            if layer.weight is not None and layer.weight.grad is not None:
+                v_old = self.velocities[layer]['w']
+                grad = layer.weight.grad.data
+                
+                v_new = (self.momentum * v_old) - (self.learning_rate * grad)
+                
+                self.velocities[layer]['w'] = v_new
+                layer.weight.data += v_new
+
+            if layer.need_bias and layer.bias is not None and layer.bias.grad is not None:
+                v_old_b = self.velocities[layer]['b']
+                grad_b = layer.bias.grad.data
+                
+                v_new_b = (self.momentum * v_old_b) - (self.learning_rate * grad_b)
+                
+                self.velocities[layer]['b'] = v_new_b
+                layer.bias.data += v_new_b
+
+    def zero_grad(self):
+         for layer in self.layers:
+            layer.zero_grad()
