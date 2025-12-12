be168b02c498aa25286ccf01bcf9a504612a277d
hiiiambobby <saba3st@gmail.com>
2025-12-12 17:19:53 +0330

    adam updated

--- optimizer/adam.py
+++ optimizer/adam.py
@@ -2,48 +2,53 @@ from mytorch.optimizer import Optimizer
 import numpy as np
 "TODO: (optional) implement Adam optimizer"
 
-
 class Adam(Optimizer):
-    def __init__(self, layers, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+    def __init__(self, layers, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8, weight_decay=0.0):
         super().__init__(layers)
         self.learning_rate = learning_rate
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
-
-        for layer in self.layers:
-            layer.first_moment = np.zeros_like(layer.weight.data)
-            layer.second_moment = np.zeros_like(layer.weight.data)
-            if layer.need_bias:
-                layer.bfirst_moment = np.zeros_like(layer.bias.data)
-                layer.bsecond_moment = np.zeros_like(layer.bias.data)
-
+        self.weight_decay = weight_decay
+        
         self.t = 0
+        self.moments = {} 
 
     def step(self):
         self.t += 1
         for layer in self.layers:
-            layer.first_moment = self.beta1 * layer.first_moment + \
-                                 (1 - self.beta1) * layer.weight.grad
-            layer.second_moment = self.beta2 * layer.second_moment + \
-                                  (1 - self.beta2) * layer.weight.grad ** 2
+            if layer not in self.moments:
+                self.moments[layer] = {
+                    'm_w': np.zeros_like(layer.weight.data) if layer.weight is not None else None,
+                    'v_w': np.zeros_like(layer.weight.data) if layer.weight is not None else None,
+                    'm_b': np.zeros_like(layer.bias.data) if layer.need_bias and layer.bias is not None else None,
+                    'v_b': np.zeros_like(layer.bias.data) if layer.need_bias and layer.bias is not None else None
+                }
 
-            m1_hat = layer.first_moment / (1 - self.beta1 ** self.t)
-            m2_hat = layer.second_moment / (1 - self.beta2 ** self.t)
+            if layer.weight is not None and layer.weight.grad is not None:
+                grad = layer.weight.grad.data + (self.weight_decay * layer.weight.data)
+                
+                self.moments[layer]['m_w'] = (self.beta1 * self.moments[layer]['m_w']) + \
+                                             ((1 - self.beta1) * grad)
+                
+                self.moments[layer]['v_w'] = (self.beta2 * self.moments[layer]['v_w']) + \
+                                             ((1 - self.beta2) * np.square(grad))
+                
+                m_hat = self.moments[layer]['m_w'] / (1 - self.beta1 ** self.t)
+                v_hat = self.moments[layer]['v_w'] / (1 - self.beta2 ** self.t)
+                
+                layer.weight.data -= self.learning_rate * (m_hat / (np.sqrt(v_hat) + self.epsilon))
 
-            layer.weight.data = layer.weight.data - self.learning_rate * \
-                                (m1_hat / (m2_hat ** 0.5 + self.epsilon))
-
-            if layer.need_bias:
-                layer.bfirst_moment = self.beta1 * layer.bfirst_moment + \
-                                      (1 - self.beta1) * layer.bias.grad
-                layer.bsecond_moment = self.beta2 * layer.bsecond_moment + \
-                                       (1 - self.beta2) * layer.bias.grad ** 2
-
-                m1b_hat = layer.bfirst_moment / \
-                          (1 - self.beta1 ** self.t)
-                m2b_hat = layer.bsecond_moment / \
-                          (1 - self.beta2 ** self.t)
-
-                layer.bias.data = layer.bias.data - self.learning_rate * \
-                                  (m1b_hat / (m2b_hat ** 0.5 + self.epsilon))
+            if layer.need_bias and layer.bias is not None and layer.bias.grad is not None:
+                grad_b = layer.bias.grad.data
+                
+                self.moments[layer]['m_b'] = (self.beta1 * self.moments[layer]['m_b']) + \
+                                             ((1 - self.beta1) * grad_b)
+                
+                self.moments[layer]['v_b'] = (self.beta2 * self.moments[layer]['v_b']) + \
+                                             ((1 - self.beta2) * np.square(grad_b))
+                
+                m_hat_b = self.moments[layer]['m_b'] / (1 - self.beta1 ** self.t)
+                v_hat_b = self.moments[layer]['v_b'] / (1 - self.beta2 ** self.t)
+                
+                layer.bias.data -= self.learning_rate * (m_hat_b / (np.sqrt(v_hat_b) + self.epsilon))
